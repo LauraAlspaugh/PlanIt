96f0edfa6820419386aea08247b675b89f044a06
Laura <lauraalspaugh2002@icloud.com>
2023-11-21 20:08:44 -0700

    models

---
+++ server/src/models/Project.js
@@ -0,0 +1,15 @@
+import { Schema } from "mongoose";
+
+export const ProjectSchema = new Schema({
+    name: { type: String, required: true, maxLength: 400 },
+    description: { type: String, required: true, maxLength: 700 },
+    creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' }
+},
+    { timestamps: true, toJSON: { virtuals: true } }
+)
+ProjectSchema.virtual('creator', {
+    localField: 'creatorId',
+    foreignField: '_id',
+    justOne: true,
+    ref: 'Account'
+})
---
+++ server/src/models/Sprint.js
@@ -0,0 +1,22 @@
+import { Schema } from "mongoose";
+
+export const SprintSchema = new Schema({
+    name: { type: String, required: true, maxLength: 300 },
+    projectId: { type: Schema.Types.ObjectId, required: true, ref: 'Project' },
+    creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
+    isOpen: { type: Boolean, required: false, default: true }
+},
+    { timestamps: true, toJSON: { virtuals: true } }
+)
+SprintSchema.virtual('creator', {
+    localField: 'creatorId',
+    foreignField: '_id',
+    justOne: true,
+    ref: 'Account'
+})
+SprintSchema.virtual('project', {
+    localField: 'projectId',
+    foreignField: '_id',
+    justOne: true,
+    ref: 'Project'
+})
