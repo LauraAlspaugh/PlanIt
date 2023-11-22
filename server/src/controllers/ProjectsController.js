8f8d66c3993dcdd3b043ea25d65d02f0f7f4afd6
Laura <lauraalspaugh2002@icloud.com>
2023-11-21 21:24:36 -0700

    more work

--- server/src/controllers/ProjectsController.js
+++ server/src/controllers/ProjectsController.js
@@ -1,8 +1,57 @@
+import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController.js";
+import { projectsService } from "../services/ProjectsService.js";
 
 export class ProjectsController extends BaseController {
     constructor() {
         super('api/projects')
         this.router
+            .get('', this.getProjects)
+            .get('/:projectId', this.getProjectById)
+            .use(Auth0Provider.getAuthorizedUserInfo)
+            .post('', this.createProject)
+            .delete('/:projectId', this.destroyProject)
+    }
+    async destroyProject(request, response, next) {
+        try {
+            const projectId = request.params.projectId
+            const userId = request.userInfo.id
+            const project = await projectsService.destroyProject(projectId, userId)
+            return response.send(project)
+        } catch (error) {
+            next(error)
+
+        }
+    }
+    async getProjectById(request, response, next) {
+        try {
+            const projectId = request.params.projectId
+            const project = await projectsService.getProjectById(projectId)
+            return response.send(project)
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getProjects(request, response, next) {
+        try {
+
+            const projects = await projectsService.getProjects()
+            return response.send(projects)
+        } catch (error) {
+            next(error)
+
+        }
+    }
+    async createProject(request, response, next) {
+        try {
+            const projectData = request.body
+            const userId = request.userInfo.id
+            projectData.creatorId = userId
+            const project = await projectsService.createProject(projectData)
+            return response.send(project)
+        } catch (error) {
+            next(error)
+
+        }
     }
 }
--- server/src/services/ProjectsService.js
+++ server/src/services/ProjectsService.js
@@ -1,4 +1,31 @@
+import { query } from "express"
+import { dbContext } from "../db/DbContext.js"
+import { BadRequest, Forbidden } from "../utils/Errors.js"
+
 class ProjectsService {
+    async destroyProject(projectId, userId) {
+        const project = await this.getProjectById(projectId)
+        if (project.creatorId.toString() != userId) {
+            throw new Forbidden('Not your project to destroy!')
+        } await project.delete()
+        return "project has been deleted!"
+    }
+    async getProjectById(projectId) {
+        const project = (await dbContext.Projects.findById(projectId)).populate('creator', 'name picture email ')
+        if (!project) {
+            throw new BadRequest('This is not a valid project id')
+        }
+        return project
+    }
+    async getProjects() {
+        const projects = await dbContext.Projects.find().populate('creator', 'name picture email')
+        return projects
+    }
+    async createProject(projectData) {
+        const project = await dbContext.Projects.create(projectData)
+        await project.populate('creator', 'name picture email')
+        return project
+    }
 
 }
 
