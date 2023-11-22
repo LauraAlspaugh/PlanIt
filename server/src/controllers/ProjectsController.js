f83e06fd9d5d6d3ca845aebc368ea972779d158a
Laura <lauraalspaugh2002@icloud.com>
2023-11-21 20:34:26 -0700

    services and controllers

---
+++ server/src/controllers/ProjectsController.js
@@ -0,0 +1,8 @@
+import BaseController from "../utils/BaseController.js";
+
+export class ProjectsController extends BaseController {
+    constructor() {
+        super('api/projects')
+        this.router
+    }
+}
---
+++ server/src/controllers/SprintsController.js
@@ -0,0 +1,8 @@
+import BaseController from "../utils/BaseController.js";
+
+export class SprintsController extends BaseController {
+    constructor() {
+        super('api/sprints')
+        this.router
+    }
+}
